e93ed0d1bb21e5e172b067cf9a8ca05b07919499
akmal <63574967+akmal203@users.noreply.github.com>
2025-03-21 20:21:55 +0700

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,8 +1,8 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_TELEGRAM_BOT_TOKEN: string
-  readonly VITE_TELEGRAM_CHAT_ID: string
+  readonly VITE_TELEGRAM_BOT_TOKEN:7662277377:AAFaXz6G_3NuFIgbX8XONwaEEPqtoKtLiL4
+  readonly VITE_TELEGRAM_CHAT_ID:-1236786198
 }
 
 interface ImportMeta {
